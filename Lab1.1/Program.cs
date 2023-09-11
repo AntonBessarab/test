a1f929bbbee94327393cbf8b37e43ce393505a96
anton <anton@DESKTOP-L26HSA4>
2023-09-11 17:11:55 +0300

    Добавьте файлы проекта.

---
+++ Lab1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Lab1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random1 = new Random(1234) ;
+
+            for (int i = 0; i < 10; i++) 
+            { 
+                int rnd1 = random1.Next(0 , 10);
+
+                Console.WriteLine(rnd1);
+             
+            }
+        }
+    }
+}
---
+++ Lab1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Lab1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rndNumberGenerator = new RNGCryptoServiceProvider();
+            var randomNumber = new byte[3];
+            rndNumberGenerator.GetBytes(randomNumber);
+            string base64String = Convert.ToBase64String(randomNumber);
+            Console.WriteLine(base64String);
+        }
+    }
+}
